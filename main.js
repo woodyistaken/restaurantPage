b6cfa5a3a7a1b81d3b962a23015352006120b16c
woodyistaken <woodyaung2005@gmail.com>
2025-06-22 23:01:54 -0700

    Deployment commit

---
+++ main.js
@@ -0,0 +1,116 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/contact.js":
+/*!************************!*\
+  !*** ./src/contact.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   contact: () => (/* binding */ contact)\n/* harmony export */ });\nfunction contact(){\n  let content=document.querySelector(\"#content\")\n  let title=document.createElement(\"h1\")\n  title.textContent=\"Best Contact Page\"\n  content.appendChild(title)\n  let p=document.createElement(\"p\")\n  p.textContent=\"This is a place which you can totally contact :p\";\n  content.appendChild(p)\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvY29udGFjdC5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQU87QUFDUDtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudHBhZ2UvLi9zcmMvY29udGFjdC5qcz9kNTY1Il0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBjb250YWN0KCl7XG4gIGxldCBjb250ZW50PWRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIjY29udGVudFwiKVxuICBsZXQgdGl0bGU9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImgxXCIpXG4gIHRpdGxlLnRleHRDb250ZW50PVwiQmVzdCBDb250YWN0IFBhZ2VcIlxuICBjb250ZW50LmFwcGVuZENoaWxkKHRpdGxlKVxuICBsZXQgcD1kb2N1bWVudC5jcmVhdGVFbGVtZW50KFwicFwiKVxuICBwLnRleHRDb250ZW50PVwiVGhpcyBpcyBhIHBsYWNlIHdoaWNoIHlvdSBjYW4gdG90YWxseSBjb250YWN0IDpwXCI7XG4gIGNvbnRlbnQuYXBwZW5kQ2hpbGQocClcbn0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/contact.js\n");
+
+/***/ }),
+
+/***/ "./src/home.js":
+/*!*********************!*\
+  !*** ./src/home.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   home: () => (/* binding */ home)\n/* harmony export */ });\nfunction home(){\n  let content=document.querySelector(\"#content\")\n  let title=document.createElement(\"h1\")\n  title.textContent=\"Best Home Page\"\n  content.appendChild(title)\n  let p=document.createElement(\"p\")\n  p.textContent=\"This is the best restaurant ever\";\n  content.appendChild(p)\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaG9tZS5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQU87QUFDUDtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudHBhZ2UvLi9zcmMvaG9tZS5qcz83YjQxIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBob21lKCl7XG4gIGxldCBjb250ZW50PWRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIjY29udGVudFwiKVxuICBsZXQgdGl0bGU9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImgxXCIpXG4gIHRpdGxlLnRleHRDb250ZW50PVwiQmVzdCBIb21lIFBhZ2VcIlxuICBjb250ZW50LmFwcGVuZENoaWxkKHRpdGxlKVxuICBsZXQgcD1kb2N1bWVudC5jcmVhdGVFbGVtZW50KFwicFwiKVxuICBwLnRleHRDb250ZW50PVwiVGhpcyBpcyB0aGUgYmVzdCByZXN0YXVyYW50IGV2ZXJcIjtcbiAgY29udGVudC5hcHBlbmRDaGlsZChwKVxufSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/home.js\n");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _home_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.js */ \"./src/home.js\");\n/* harmony import */ var _menu_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./menu.js */ \"./src/menu.js\");\n/* harmony import */ var _contact_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./contact.js */ \"./src/contact.js\");\n\n\n\nfunction clear(){\n  document.querySelector(\"#content\").innerHTML=\"\"\n}\n\ndocument.querySelector(\".home\").addEventListener(\"click\",()=>{clear();(0,_home_js__WEBPACK_IMPORTED_MODULE_0__.home)();})\ndocument.querySelector(\".menu\").addEventListener(\"click\",()=>{clear();(0,_menu_js__WEBPACK_IMPORTED_MODULE_1__.menu)();})\ndocument.querySelector(\".contact\").addEventListener(\"click\",()=>{clear();(0,_contact_js__WEBPACK_IMPORTED_MODULE_2__.contact)();})//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7OztBQUE4QjtBQUNBO0FBQ007QUFDcEM7QUFDQTtBQUNBOztBQUVBLDhEQUE4RCxRQUFRLDhDQUFJLElBQUk7QUFDOUUsOERBQThELFFBQVEsOENBQUksSUFBSTtBQUM5RSxpRUFBaUUsUUFBUSxvREFBTyxJQUFJIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudHBhZ2UvLi9zcmMvaW5kZXguanM/YjYzNSJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQge2hvbWV9IGZyb20gXCIuL2hvbWUuanNcIlxuaW1wb3J0IHttZW51fSBmcm9tIFwiLi9tZW51LmpzXCJcbmltcG9ydCB7Y29udGFjdH0gZnJvbSBcIi4vY29udGFjdC5qc1wiXG5mdW5jdGlvbiBjbGVhcigpe1xuICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwiI2NvbnRlbnRcIikuaW5uZXJIVE1MPVwiXCJcbn1cblxuZG9jdW1lbnQucXVlcnlTZWxlY3RvcihcIi5ob21lXCIpLmFkZEV2ZW50TGlzdGVuZXIoXCJjbGlja1wiLCgpPT57Y2xlYXIoKTtob21lKCk7fSlcbmRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIubWVudVwiKS5hZGRFdmVudExpc3RlbmVyKFwiY2xpY2tcIiwoKT0+e2NsZWFyKCk7bWVudSgpO30pXG5kb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwiLmNvbnRhY3RcIikuYWRkRXZlbnRMaXN0ZW5lcihcImNsaWNrXCIsKCk9PntjbGVhcigpO2NvbnRhY3QoKTt9KSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ }),
+
+/***/ "./src/menu.js":
+/*!*********************!*\
+  !*** ./src/menu.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   menu: () => (/* binding */ menu)\n/* harmony export */ });\nfunction menu(){\n  let content=document.querySelector(\"#content\")\n  let title=document.createElement(\"h1\")\n  title.textContent=\"Best Menu\"\n  content.appendChild(title)\n  let p=document.createElement(\"p\")\n  p.textContent=\"We only have generic food for 1 dollar :(\";\n  content.appendChild(p)\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbWVudS5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQU87QUFDUDtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudHBhZ2UvLi9zcmMvbWVudS5qcz9hYjA4Il0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBmdW5jdGlvbiBtZW51KCl7XG4gIGxldCBjb250ZW50PWRvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoXCIjY29udGVudFwiKVxuICBsZXQgdGl0bGU9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImgxXCIpXG4gIHRpdGxlLnRleHRDb250ZW50PVwiQmVzdCBNZW51XCJcbiAgY29udGVudC5hcHBlbmRDaGlsZCh0aXRsZSlcbiAgbGV0IHA9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcInBcIilcbiAgcC50ZXh0Q29udGVudD1cIldlIG9ubHkgaGF2ZSBnZW5lcmljIGZvb2QgZm9yIDEgZG9sbGFyIDooXCI7XG4gIGNvbnRlbnQuYXBwZW5kQ2hpbGQocClcbn0iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/menu.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
